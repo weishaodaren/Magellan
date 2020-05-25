57aa3f4b6d033da99a4c8e9b781870d1f39c8910
weishaodaren <zhaojiawei@realwaychina.com>
2020-05-25 13:22:25 +0800

    play with TS FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = { message: string }
+
+// const App = ({ message }: AppProps) => <div>{message}</div>
+// const App: React.FC<{ message: string }> = ({ message }) => <div>{message}</div>
+const App: React.FunctionComponent<{ title: string }> = ({
+    children,
+    title,
+}) => {
+    return <div title={title}>{title}</div>
 }
 
-export default App;
+export default App
